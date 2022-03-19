37bc720725459206026734b6f28323fae038f9e0
adrianetodesign <adrianetodesign@gmail.com>
2022-03-19 15:06:24 -0700

    Create Navbar component

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Party</li>
+        <li>Intiative</li>
+        <li>Notes</li>
+        <li>Tables</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
