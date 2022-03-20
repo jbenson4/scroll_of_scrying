5e2ca9cc5479200240d4badd97f5845383af98af
Jordan <jordan.benson4@gmail.com>
2022-03-19 19:29:20 -0700

    Create player query promises.

---
+++ server/db/queries/player_queries.sql
@@ -0,0 +1,50 @@
+// gets a single player from the databse by a player ID
+const getPlayerWithId = function (id, pool) {
+  return pool.query(`
+  SELECT *
+  FROM players
+  WHERE players.id = $1;
+  `, [id])
+    .then((response) => {
+      if (response.rows[0].length === 0) { return null }
+      else { return response.rows[0] }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+exports.getPlayerWithId = getPlayerWithId;
+
+// gets all player data from the database
+const getAllPlayerData = function (id, pool) {
+  return pool.query(`
+  SELECT *
+  FROM players;
+  `)
+    .then((response) => {
+      if (response.rows[0].length === 0) { return null }
+      else { return response.rows; }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+exports.getAllPlayerData = getAllPlayerData;
+
+const getPlayerConditions = function (id, pool) {
+  return pool.query(`
+  SELECT conditions.name
+  FROM players_conditions_ownership
+  JOIN players ON player_id = players.id
+  JOIN conditions ON conditions.id = players_conditions_ownership.condition_id
+  WHERE players.id = $1;
+  `, [id])
+    .then((response) => {
+      if (response.rows[0].length === 0) { return null }
+      else { return response.rows }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+exports.getPlayerConditions = getPlayerConditions;
