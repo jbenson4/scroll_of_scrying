a9f17cf21bbdf96205a88eb66bd1887a41578ca7
Jordan <jordan.benson4@gmail.com>
2022-03-19 19:27:44 -0700

    Create condition seed data.

---
+++ server/db/seeds/03_conditions.sql
@@ -0,0 +1,4 @@
+INSERT INTO conditions (player_id, index, name) VALUES (1, 'poisoned', 'Poisoned');
+INSERT INTO conditions (player_id, index, name) VALUES (1, 'blinded', 'Blinded');
+INSERT INTO conditions (player_id, index, name) VALUES (3, 'poisoned', 'Poisoned');
+INSERT INTO conditions (player_id, index, name) VALUES (4, 'poisoned', 'Poisoned');
