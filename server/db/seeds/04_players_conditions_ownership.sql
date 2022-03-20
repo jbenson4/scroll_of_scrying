a7bde2173a677093f4a691e96d5f8df81ac2f8cd
Jordan <jordan.benson4@gmail.com>
2022-03-19 19:28:03 -0700

    Create player condition join table seed data.

---
+++ server/db/seeds/04_players_conditions_ownership.sql
@@ -0,0 +1,4 @@
+INSERT INTO players_conditions_ownership (player_id, condition_id) VALUES (1, 1);
+INSERT INTO players_conditions_ownership (player_id, condition_id) VALUES (1, 2);
+INSERT INTO players_conditions_ownership (player_id, condition_id) VALUES (3, 3);
+INSERT INTO players_conditions_ownership (player_id, condition_id) VALUES (4, 4);
