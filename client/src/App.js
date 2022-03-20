258b19cc2ab95ee0d92e9c28c44be656e3f32420
adrianetodesign <adrianetodesign@gmail.com>
2022-03-20 10:33:11 -0700

    Add import for PlayerList

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.scss';
 import Navbar from './components/Navbar';
+import PlayerList from './components/PlayerList';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <section>
+        <PlayerList />
+      </section>
     </div>
   );
 }
