8c287ecc68455d37e5de745d989f182170188af6
Jordan <jordan.benson4@gmail.com>
2022-03-19 19:26:10 -0700

    Create database schema.

---
+++ server/db/schema/01_schema.sql
@@ -0,0 +1,45 @@
+-- Drop Tables if existant
+DROP TABLE IF EXISTS players CASCADE;
+DROP TABLE IF EXISTS items CASCADE;
+DROP TABLE IF EXISTS conditions CASCADE;
+DROP TABLE IF EXISTS players_conditions_ownership CASCADE;
+
+-- Recreate Tables
+----------------------------------------
+
+-- Recreate players Table
+CREATE TABLE players (
+  id SERIAL PRIMARY KEY NOT NULL,
+
+  name VARCHAR(255) NOT NULL,
+  race TEXT,
+  class TEXT,
+  stats JSON,
+  condition_id INTEGER,
+  level INTEGER
+);
+
+-- Recreate items Table
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY NOT NULL,
+
+  index TEXT,
+  name TEXT
+);
+
+-- Recreate conditions Table
+CREATE TABLE conditions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  player_id INTEGER REFERENCES players(id) ON DELETE CASCADE,
+
+  index TEXT,
+  name TEXT,
+  description TEXT
+);
+
+-- Recreate players_conditions_ownership Join Table
+CREATE TABLE players_conditions_ownership (
+  id SERIAL PRIMARY KEY NOT NULL,
+  player_id INTEGER REFERENCES players(id) ON DELETE CASCADE,
+  condition_id INTEGER REFERENCES conditions(id) ON DELETE CASCADE
+);
