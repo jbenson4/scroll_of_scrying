acd1952a86128785456473c24ab0d13518c960a8
Jordan <jordan.benson4@gmail.com>
2022-03-19 19:27:09 -0700

    Create player seed data.

---
+++ server/db/seeds/01_players.sql
@@ -0,0 +1,54 @@
+INSERT INTO players (name, race, class ,stats, condition_id, level) VALUES ('Lezana Carlucci', 'human', 'paladin', '{
+  "strength": 14,
+  "dexterity": 10,
+  "constitution": 16,
+  "intelligence": 10,
+  "wisdom": 12,
+  "charisma": 18,
+  "hp": 81
+}', 1, 8);
+INSERT INTO players (name, race, class ,stats, condition_id, level) VALUES ('Rorin Grimleeper', 'dwarf', 'fighter', '{
+  "strength": 16,
+  "dexterity": 12,
+  "constitution": 14,
+  "intelligence": 16,
+  "wisdom": 18,
+  "charisma": 12,
+  "hp": 90
+}', 2, 9);
+INSERT INTO players (name, race, class ,stats, condition_id, level) VALUES ('Halyassa Diltheth', 'dragonborn', 'barbarian', '{
+  "strength": 15,
+  "dexterity": 13,
+  "constitution": 14,
+  "intelligence": 14,
+  "wisdom": 14,
+  "charisma": 14,
+  "hp": 53
+}', 3, 5);
+INSERT INTO players (name, race, class ,stats, condition_id, level) VALUES ('Nym Silveroak', 'elf', 'ranger', '{
+  "strength": 14,
+  "dexterity": 16,
+  "constitution": 14,
+  "intelligence": 14,
+  "wisdom": 15,
+  "charisma": 16,
+  "hp": 58
+}', 4, 5);
+INSERT INTO players (name, race, class ,stats, condition_id, level) VALUES ('Flambard Kaese', 'halfling', 'wizard', '{
+  "strength": 12,
+  "dexterity": 12,
+  "constitution": 16,
+  "intelligence": 18,
+  "wisdom": 18,
+  "charisma": 13,
+  "hp": 61
+}', 5, 6);
+INSERT INTO players (name, race, class ,stats, condition_id, level) VALUES ('Marlypsis Grantham', 'tiefling', 'druid', '{
+  "strength": 14,
+  "dexterity": 13,
+  "constitution": 14,
+  "intelligence": 15,
+  "wisdom": 16,
+  "charisma": 14,
+  "hp": 75
+}', 6, 7);
