c13251e38cb0691bf84572611904e87226644ad3
adrianetodesign <adrianetodesign@gmail.com>
2022-03-20 15:39:30 -0700

    Add import for PlayerList

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.scss';
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 import PlayerList from './components/PlayerList';
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Navbar />
       <section>
         <PlayerList />
