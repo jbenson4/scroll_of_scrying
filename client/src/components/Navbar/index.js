3f9df9a9f9baa63b61099f843d0eb1f06e9cf24c
adrianetodesign <adrianetodesign@gmail.com>
2022-03-21 14:45:30 -0700

    Add setSelectedTab prop to Navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Navbar.scss';
 
-const Navbar = () => {
+const Navbar = ({setSelectedTab}) => {
   return (
     <div class="navbar">
       <span id="party-name">Company of the Yellow Banner</span>
       <nav>
         <ul>
-          <li><button>Party</button></li>
-          <li><button>Combat</button></li>
-          <li><button>Notes</button></li>
-          <li><button>Tables</button></li>
+          <li><button onClick={() => setSelectedTab('party')}>Party</button></li>
+          <li><button onClick={() => setSelectedTab('combat')}>Combat</button></li>
+          <li><button onClick={() => setSelectedTab('notes')}>Notes</button></li>
+          <li><button onClick={() => setSelectedTab('tables')}>Tables</button></li>
         </ul>
       </nav>
     </div>
