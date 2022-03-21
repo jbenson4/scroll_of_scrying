57711944983746df136fb9f7481bc8b6c4c22bc1
Jordan <jordan.benson4@gmail.com>
2022-03-21 12:20:32 -0700

    Create PUT route to edit player.

--- server/routers/player-router.js
+++ server/routers/player-router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const playerQueries = require('../db/queries/player_queries.js');
+const { editPlayer } = require('../db/updates/player_updates.js');
 
 module.exports = (db) => {
 
@@ -19,5 +20,20 @@ module.exports = (db) => {
         res.send(data);
       });
   });
+
+  // PUT routes
+  router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const editedPlayer = req.body;
+    editedPlayer.id = id;
+    editPlayer(editedPlayer, db)
+      .then((data) => {
+        res.send(data);
+      })
+  });
+
+  // router.put('/conditions/:id', (req, res) => {
+  //   const editedConditions = req.body;
+  // });
   return router;
 };
