78a8c04e6511803e486e9a15a309d03188fa71a4
Jordan <jordan.benson4@gmail.com>
2022-03-21 14:54:08 -0700

    Add get party items and delete item queries.

--- server/db/queries/party-queries.js
+++ server/db/queries/party-queries.js
@@ -1,5 +1,5 @@
 // gets all party data
-const getPartyData = function(pool) {
+const getPartyData = function (pool) {
   return pool
     .query(
       `
@@ -19,3 +19,45 @@ const getPartyData = function(pool) {
     });
 };
 exports.getPartyData = getPartyData;
+
+const getPartyItems = function (pool) {
+  return pool
+    .query(
+      `
+      SELECT *
+      FROM items;
+      `
+    )
+    .then((response) => {
+      if (response.rows[0].length === 0) {
+        return null;
+      } else {
+        return response.rows;
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+exports.getPartyItems = getPartyItems;
+
+const destroyItem = function (id, pool) {
+  return pool
+    .query(
+      `
+      DELETE FROM items
+      WHERE id = $1;
+      `
+    , [id])
+    .then((response) => {
+      if (response.rows[0].length === 0) {
+        return null;
+      } else {
+        return response.rows;
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+exports.destroyItem = destroyItem;
