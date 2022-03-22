22540b6c1ab5ad3689a3dd0fe782fce4e5b200ec
adrianetodesign <adrianetodesign@gmail.com>
2022-03-21 19:48:20 -0700

    Update Inventory to use item component

--- client/src/components/Inventory.js
+++ client/src/components/Inventory.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Item from './Item';
+
+const itemsData = [
+  {index: 'adamantine-armor', name: 'Adamantine Armor'},
+  {index: 'crystal-ball', name: 'Crystal Ball'},
+  {index: 'goggles-of-night', name: 'Goggles of Night'},
+  {index: 'ring-of-invisibility', name: 'Ring of Invisibility'}
+]
+
+const parsedItems = itemsData.map(item => <Item key={item.name} {...item}/>)
 
 const Inventory = () => {
   return (
-    <div>Inventory</div>
+    <>
+      { parsedItems }
+    </>
   )
 }
 
