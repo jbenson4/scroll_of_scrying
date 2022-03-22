de0cfd8bb4dab5c0aa452218d7da5950bd4047b1
adrianetodesign <adrianetodesign@gmail.com>
2022-03-21 19:47:52 -0700

    Create new Party Component for tabs

---
+++ client/src/components/Party.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PlayerList from './PlayerList';
+import Inventory from './Inventory';
+
+const Party = () => {
+  return (
+    <div>
+      <PlayerList />
+      <Inventory />
+    </div>
+  )
+}
+
+export default Party
