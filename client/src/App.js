4c56fa84a252ae8902bd62762be3d18dc1570176
adrianetodesign <adrianetodesign@gmail.com>
2022-03-21 19:46:52 -0700

    Add tabs state to App

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,15 @@ import React, { useState } from 'react';
 import './App.scss';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
-import PlayerList from './components/PlayerList';
-import Inventory from './components/Inventory';
+import Party from './components/Party';
 import Dice from './components/Dice';
 
 
 const TABS = {
-  'party': <PlayerList />,
-  'combat': <Inventory />,
-  'notes': <Inventory />,
-  'tables': <Inventory />
+  'party': <Party />,
+  'combat': <Dice />,
+  'notes': <Dice />,
+  'tables': <Dice />
 }
 
 function App() {
