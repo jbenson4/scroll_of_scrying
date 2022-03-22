b2b62263b44525b621a27421f3c758e8c600c61d
Jordan <jordan.benson4@gmail.com>
2022-03-22 10:07:23 -0700

    Create seperate player deletion file.

--- server/db/deletions/player_deletion.js
+++ server/db/deletions/player_deletion.js
@@ -1,19 +1,20 @@
 // removes a player condition
-// const removeCondition = function (id, index, pool) {
-//   return pool.query(
-//     `
-//     DELETE FROM conditions
-//     WHERE player_id = $1
-//     AND index = $2;
-//     `
-//   , [id, index])
-//   .then((result) => {
-//     return result.rows[0]
-//   })
-//   .catch((err) => {
-//     console.log(err.message);
-//   });
-// };
+const removeCondition = function (id, index, pool) {
+  return pool.query(
+    `
+    DELETE FROM conditions
+    WHERE player_id = $1
+    AND index = $2;
+    `
+  , [id, index])
+  .then((result) => {
+    return result.rows[0]
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+};
+exports.removeCondition = removeCondition;
 
 // destroys a player instance
 const destroyPlayer = function (id, pool) {
