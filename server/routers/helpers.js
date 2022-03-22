988f8801151c3baf47365256095009b65cf81873
Jordan <jordan.benson4@gmail.com>
2022-03-22 09:09:59 -0700

    Create item addition helper function.

--- server/routers/helpers.js
+++ server/routers/helpers.js
@@ -1,5 +1,6 @@
 const { default: axios } = require("axios");
 const { addPlayerCondition } = require("../db/insertions/condition_insertion");
+const { addPartyItem } = require("../db/insertions/item_insertion");
 
 
 const playerConditionHelper = function (db, id, index) {
@@ -20,6 +21,24 @@ const playerConditionHelper = function (db, id, index) {
       .catch((err) => console.log(err.message))
 }
 
+const itemHelper = function (db, index) {
+  const formatted = index.trim().replace(/\s+/g, '-').toLowerCase();
+  axios.get(`https://www.dnd5eapi.co/api/magic-items/${formatted}`)
+      .then((response) => {
+        const description = response.data.desc.join(' ');
+        const values = [
+          response.data.index,
+          response.data.name,
+          description
+        ];
+        addPartyItem(values, db)
+          .then((res) => console.log(res))
+          .catch((err) => console.log(err))
+      })
+      .catch((err) => console.log(err.message))
+}
+
 module.exports = {
-  playerConditionHelper: playerConditionHelper
+  playerConditionHelper,
+  itemHelper
 };
