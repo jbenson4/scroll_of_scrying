e1802b498cc5f9e4ec8107dd4fddeaa7f40ce1d4
Jordan <jordan.benson4@gmail.com>
2022-03-23 16:14:38 -0700

    Create state and use effect call to generate random monsters.

--- client/src/components/Tables.js
+++ client/src/components/Tables.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import TableRow from './TableRow'
+import axios from 'axios';
 
 const Tables = () => {
-  const obj = [<TableRow />]
+  const [ categoryItems, setCategoryItems ] = useState();
+  const num = 10;
+  useEffect(() => {
+
+    axios.get('https://www.dnd5eapi.co/api/monsters')
+    .then((res) => {
+      // console.log('res: ', res.data)
+      const array = res.data.results;
+      const items = [];
+      const tableLength = num;
+      while (items.length < tableLength) {
+        const randomElement = array[Math.floor(Math.random() * array.length)]
+        items.push(randomElement);
+      }
+      const itemsArray = items.map((item) => (
+        <TableRow name={item.name} />
+        ))
+        setCategoryItems(itemsArray);
+      })
+      .catch((err) => console.log(err))
+    }, []);
   return (
     <div>
       Tables
-      <TableRow />
-      <TableRow />
-      <TableRow />
+      <ul>
+      {categoryItems}
+      </ul>
     </div>
   )
 }
