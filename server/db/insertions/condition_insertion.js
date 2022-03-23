89bc066a23ce070b8296638afbe99d48011fc2ed
Jordan <jordan.benson4@gmail.com>
2022-03-22 18:12:38 -0700

    Remove redundant player_id column.

--- server/db/insertions/condition_insertion.js
+++ server/db/insertions/condition_insertion.js
@@ -1,7 +1,7 @@
 const addPlayerCondition = function (values, pool) {
   return pool.query(`
-    INSERT INTO conditions (player_id, index, name, description)
-    VALUES ($1, $2, $3, $4)
+    INSERT INTO conditions (index, name, description)
+    VALUES ($1, $2, $3)
     RETURNING *;
   `, values)
 };
