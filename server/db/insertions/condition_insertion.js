7fbfc26ec068002ce3c2ad123bab41dabef4d56a
Jordan <jordan.benson4@gmail.com>
2022-03-23 13:29:12 -0700

    Refactor query to use array of values inside of file.

--- server/db/insertions/condition_insertion.js
+++ server/db/insertions/condition_insertion.js
@@ -1,11 +1,11 @@
-const addPlayerCondition = function (values, pool) {
+const addPlayerCondition = function (id, index, pool) {
   return pool.query(
     `
   INSERT INTO players_conditions (player_id, condition_id)
   VALUES ($1, (SELECT conditions.id FROM conditions WHERE conditions.index = $2))
   RETURNING *;
   `,
-    values
+    [id, index]
   );
 };
 exports.addPlayerCondition = addPlayerCondition;
