0ebb9734504cd7f329f2cd0c3790710a6912d752
Jordan <jordan.benson4@gmail.com>
2022-03-22 18:14:09 -0700

    Create monster object function.

--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@ db.connect();
 // router variables
 const playerRoutes = require('./routers/player-router');
 const partyRoutes = require('./routers/party-router');
+const { getMonsterObject } = require("./routers/monster-router.js");
 // const monsterRoutes = require('./routers/monster-router');
 // const noteRoutes = require('./routers/note-router');
 
@@ -27,6 +28,7 @@ const partyRoutes = require('./routers/party-router');
 
 app.use('/party', partyRoutes(db));
 app.use('/players', playerRoutes(db));
+getMonsterObject("ancient-black-dragon")
 
 // listen
 app.listen(port, () => {
