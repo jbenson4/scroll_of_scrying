a4ddb7ddfbd7aec8bea77648f9cb0a4d8b3cc621
Jordan <jordan.benson4@gmail.com>
2022-03-23 16:15:47 -0700

    Create axios request to get table row item data.

--- client/src/components/TableRow.js
+++ client/src/components/TableRow.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import kebabcase from 'lodash.kebabcase';
 
-const TableRow = () => {
+const TableRow = (props) => {
+  // Strips "Form" from props name value to handle edge cases of DnD API URIs (eg. Werebear, Bear Form requires a URI of ./werebear-bear)
+  const name = props.name.replace('Form', '');
+  // Format name variable to the required kebab case for DnD API
+  const index = kebabcase(name);
+  
+  axios.get(`https://www.dnd5eapi.co/api/monsters/${index}`)
+    .then((res) => {
+      // alert(res)
+      console.log(res.data)
+    })
   return (
-    <div>Table Item</div>
+    <li>A wild <strong>{props.name}</strong> attacks you!</li>
   )
 }
 
