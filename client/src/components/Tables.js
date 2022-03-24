66aaf58d8ab08f6696dd3f8a07e6632fc3204681
Jordan <jordan.benson4@gmail.com>
2022-03-24 14:59:11 -0700

    Add breaks between UI elements.

--- client/src/components/Tables.js
+++ client/src/components/Tables.js
@@ -10,8 +10,15 @@ const Tables = () => {
       <button onClick={setTableCategory}>Monsters</button>
       <button onClick={setTableCategory}>Magic Items</button>
       <button onClick={setTableCategory}>NPCs</button>
+      <br></br>
       <label for='roll-length'>Table Length:</label>
+      <br></br>
       <input id='roll-length' type='number' defaultValue='10' onChange={setTableLength}></input>
+      <br></br>
+      <button onClick={setTableCategory}>Roll Again</button>
+      <br></br>
+      <br></br>
+
 
       {categoryItems !== undefined && categoryItems.map((item) => <TableRow key={item.id} name={item.name} />)}
     </div>
