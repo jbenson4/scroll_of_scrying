bdb18117452bc3d0492063e850f593c95a32b7ab
Jordan <jordan.benson4@gmail.com>
2022-03-24 10:28:37 -0700

    Add table length input field.

--- client/src/components/Tables.js
+++ client/src/components/Tables.js
@@ -10,6 +10,9 @@ const Tables = () => {
       <button onClick={setContext}>Monsters</button>
       <button onClick={setContext}>Magic Items</button>
       <button onClick={setContext}>NPCs</button>
+      <label for='roll-length'>Table Length:</label>
+      <input id='roll-length' type='number' value={10}></input>
+
       {categoryItems !== undefined && categoryItems.map((item) => <TableRow key={item.id} name={item.name} />)}
     </div>
   )
