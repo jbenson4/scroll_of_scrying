b1fd7b368937f705b108b69e618372f3fe12009f
Jordan <jordan.benson4@gmail.com>
2022-03-24 11:24:53 -0700

    Update on click function name.

--- client/src/components/Tables.js
+++ client/src/components/Tables.js
@@ -3,15 +3,15 @@ import TableRow from './TableRow'
 import useRollTableData from '../hooks/useRollTableData';
 
 const Tables = () => {
-  const { categoryItems, setContext } = useRollTableData();
+  const { categoryItems, setTableCategory, setTableLength } = useRollTableData();
 
   return (
     <div>
-      <button onClick={setContext}>Monsters</button>
-      <button onClick={setContext}>Magic Items</button>
-      <button onClick={setContext}>NPCs</button>
+      <button onClick={setTableCategory}>Monsters</button>
+      <button onClick={setTableCategory}>Magic Items</button>
+      <button onClick={setTableCategory}>NPCs</button>
       <label for='roll-length'>Table Length:</label>
-      <input id='roll-length' type='number' value={10}></input>
+      <input id='roll-length' type='number' defaultValue='10' onChange={setTableLength}></input>
 
       {categoryItems !== undefined && categoryItems.map((item) => <TableRow key={item.id} name={item.name} />)}
     </div>
