0e76807b7e7927fb4206a59c7811b34af2e67e1a
Jordan <jordan.benson4@gmail.com>
2022-03-23 17:14:33 -0700

    Add on click function to display alert window with data.

--- client/src/components/TableRow.js
+++ client/src/components/TableRow.js
@@ -3,18 +3,21 @@ import React, { useState, useEffect } from 'react'
 import kebabcase from 'lodash.kebabcase';
 
 const TableRow = (props) => {
-  // Strips "Form" from props name value to handle edge cases of DnD API URIs (eg. Werebear, Bear Form requires a URI of ./werebear-bear)
-  const name = props.name.replace('Form', '');
-  // Format name variable to the required kebab case for DnD API
-  const index = kebabcase(name);
-  
-  axios.get(`https://www.dnd5eapi.co/api/monsters/${index}`)
-    .then((res) => {
-      // alert(res)
-      console.log(res.data)
-    })
+  const getDetails = function (i) {
+    // Takes click target inner HTML value and strips "Form" from it to handle edge cases of DnD API URIs (eg. Werebear, Bear Form requires a URI of ./werebear-bear)
+    const name = i.target.innerText.replace('Form', '');
+    // Format name variable to the required kebab case for DnD API
+    const index = kebabcase(name);
+
+    axios.get(`https://www.dnd5eapi.co/api/monsters/${index}`)
+      .then((res) => {
+        alert(res.data.name)
+        console.log(res.data)
+
+      })
+  }
   return (
-    <li>A wild <strong>{props.name}</strong> attacks you!</li>
+    <li>A wild <strong onClick={getDetails}>{props.name}</strong> attacks you!</li>
   )
 }
 
