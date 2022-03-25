1f52d2d75c599e4bb5ea34445b133a1929f207cb
Jordan <jordan.benson4@gmail.com>
2022-03-25 15:37:15 -0700

    Add id tag to element name for modal display.

--- client/src/components/TableRow.js
+++ client/src/components/TableRow.js
@@ -3,12 +3,10 @@ import useRollTableData from '../hooks/useRollTableData';
 import useElementDetails from '../hooks/useElementDetails';
 
 const TableRow = (props) => {
-  const { details, getDetails } =  useElementDetails();
-  console.log('props: ', props)
   return (
     <Fragment>
-      {props.category.index === 'magic-items' && <li>You stumble upon a <strong onClick={props.getDetails}>{props.name}</strong>!</li>}
-      {props.category.index === 'monsters' && <li>A wild <strong onClick={props.getDetails}>{props.name}</strong> attacks you!</li>}
+      {props.category.index === 'magic-items' && <li>You stumble upon a <strong onClick={props.getDetails} id={'magic-items'}>{props.name}</strong>!</li>}
+      {props.category.index === 'monsters' && <li>A wild <strong onClick={props.getDetails} id={'monsters'}>{props.name}</strong> attacks you!</li>}
       {props.category.index === 'np-cs' && <li>{props.npc}</li>}
     </Fragment>
   )
