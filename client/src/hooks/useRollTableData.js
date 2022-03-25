a3d94e2f788610a2b5eaaf349c49fe479cdb9d90
Jordan <jordan.benson4@gmail.com>
2022-03-25 11:33:54 -0700

    Remove FCG.

--- client/src/hooks/useRollTableData.js
+++ client/src/hooks/useRollTableData.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import kebabcase from 'lodash.kebabcase';
-import FCG from "fantasy-content-generator";
 import npbff from "npbff";
 
 
