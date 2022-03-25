d15903236630740a0d5d3f5346eb21933ddab482
Jordan <jordan.benson4@gmail.com>
2022-03-25 11:32:44 -0700

    Refactor to use prop drilling.

--- client/src/components/TableRow.js
+++ client/src/components/TableRow.js
@@ -1,17 +1,15 @@
-import axios from 'axios'
 import React, { Fragment } from 'react'
 import useRollTableData from '../hooks/useRollTableData';
 import useElementDetails from '../hooks/useElementDetails';
-import kebabcase from 'lodash.kebabcase';
 
 const TableRow = (props) => {
-  const { category } = useRollTableData();
   const { details, getDetails } =  useElementDetails();
-
+  console.log('props: ', props)
   return (
     <Fragment>
-      {category.index === 'magic-items' && <li>You stumble upon a <strong onClick={getDetails}>{props.name}</strong>!</li>}
-      {category.index === 'monsters' && <li>A wild <strong onClick={getDetails}>{props.name}</strong> attacks you!</li>}
+      {props.category.index === 'magic-items' && <li>You stumble upon a <strong onClick={props.getDetails}>{props.name}</strong>!</li>}
+      {props.category.index === 'monsters' && <li>A wild <strong onClick={props.getDetails}>{props.name}</strong> attacks you!</li>}
+      {props.category.index === 'np-cs' && <li>{props.npc}</li>}
     </Fragment>
   )
 }
