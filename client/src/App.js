58439bf9ab63907da7469e69fea99eca0f9e31c2
Jordan <jordan.benson4@gmail.com>
2022-03-25 11:27:25 -0700

    Move TABS variable into App component.

--- client/src/App.js
+++ client/src/App.js
@@ -7,16 +7,40 @@ import Dice from './components/Dice';
 import Notes from './components/Notes';
 import Tables from './components/Tables';
 import PlayerProvider from './providers/PartyProvider';
+import useRollTableData from './hooks/useRollTableData';
+import useElementDetails from './hooks/useElementDetails';
 
 
-const TABS = {
-  'party': <Party />,
-  'combat': <Dice />,
-  'notes': <Notes />,
-  'tables': <Tables />
-}
+// const TABS = {
+//   'party': <Party />,
+//   'combat': <Dice />,
+//   'notes': <Notes />,
+//   'tables': <Tables
+//   category={category}
+//   setCategory={setCategory}
+//   setTableCategory={setTableCategory}
+//   setTableLength={setTableLength}
+//   categoryItems={categoryItems}
+//   getDetails={getDetails}
+//   />
+// }
 
 function App() {
+  const { category, setCategory, setTableCategory, setTableLength, categoryItems } = useRollTableData();
+  const { getDetails } = useElementDetails();
+  const TABS = {
+    'party': <Party />,
+    'combat': <Dice />,
+    'notes': <Notes />,
+    'tables': <Tables
+    category={category}
+    setCategory={setCategory}
+    setTableCategory={setTableCategory}
+    setTableLength={setTableLength}
+    categoryItems={categoryItems}
+    getDetails={getDetails}
+    />
+  }
   const [selectedTab, setSelectedTab] = useState('party');
   return (
     <PlayerProvider>
