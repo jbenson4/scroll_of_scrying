b3aa44b6eeb46097e724efb19acd360245f4b0c8
Jordan <jordan.benson4@gmail.com>
2022-03-25 11:32:29 -0700

    Refactor to use prop drilling.

--- client/src/components/Tables.js
+++ client/src/components/Tables.js
@@ -1,26 +1,26 @@
 import React from 'react'
 import TableRow from './TableRow'
-import useRollTableData from '../hooks/useRollTableData';
 
-const Tables = () => {
-  const { categoryItems, setTableCategory, setTableLength } = useRollTableData();
-
+const Tables = (props) => {
   return (
     <div>
-      <button onClick={setTableCategory}>Monsters</button>
-      <button onClick={setTableCategory}>Magic Items</button>
-      <button onClick={setTableCategory}>NPCs</button>
+      <button onClick={props.setTableCategory}>Monsters</button>
+      <button onClick={props.setTableCategory}>Magic Items</button>
+      <button onClick={props.setTableCategory}>NPCs</button>
       <br></br>
-      <label for='roll-length'>Table Length:</label>
+      <label htmlFor='roll-length'>Table Length:</label>
       <br></br>
-      <input id='roll-length' type='number' defaultValue='10' onChange={setTableLength}></input>
+      <input id='roll-length' type='number' defaultValue='10' onChange={props.setTableLength}></input>
       <br></br>
-      <button onClick={setTableCategory}>Roll Again</button>
+      <button onClick={props.setTableCategory}>Roll Again</button>
       <br></br>
       <br></br>
 
+      <ol>
 
-      {categoryItems !== undefined && categoryItems.map((item) => <TableRow key={item.id} name={item.name} />)}
+      {props.categoryItems !== undefined && props.categoryItems.map((item) => <TableRow key={item.index} name={item.name} getDetails={props.getDetails} category={props.category} />)}
+      {props.category === 'np-cs' && props.categoryItems.map((item) => <TableRow key={item} npc={item} getDetails={props.getDetails} category={props.category} />)}
+      </ol>
     </div>
   )
 }
