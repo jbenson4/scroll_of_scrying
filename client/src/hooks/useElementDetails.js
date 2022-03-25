b7feeb2bd5daa67c96c7d53161cfb0561f597cdd
Jordan <jordan.benson4@gmail.com>
2022-03-25 15:38:19 -0700

    Add modal display to state object.

--- client/src/hooks/useElementDetails.js
+++ client/src/hooks/useElementDetails.js
@@ -5,24 +5,40 @@ import kebabcase from 'lodash.kebabcase';
 
 // Custom hook for handling state between different roll table categories
 export default function useElementDetails() {
-  const [details, setDetails] = useState();
+  const [details, setDetails] = useState({
+    show: false,
+    data: {}
+  });
 
   // const setContext = (event) => {
   //   const uri = kebabcase(event.target.innerText);
   //   setCategory(uri);
   // };
+
+  const showModal = () => {
+    setDetails((prev) => ({
+      ...prev,
+      show: !details.show
+    }));
+  };
   
-  const getDetails = (i) => {
+  const getDetails = (event) => {
+    showModal()
+    const uriCategory = event.target.id;
     // Takes click target inner text value and strips "Form" from it to handle edge cases of DnD API URIs (eg. Werebear, Bear Form requires a URI of ./werebear-bear)
-    const name = i.target.innerText.replace('Form', '');
+    const name = event.target.innerText.replace('Form', '');
     // Format name variable to the required kebab case for DnD API
     const index = kebabcase(name);
-    axios.get(`https://www.dnd5eapi.co/api/monsters/${index}`)
+    axios.get(`https://www.dnd5eapi.co/api/${uriCategory}/${index}`)
       .then((res) => {
-        setDetails(res.data);
-        alert(res.data.name)
+        console.log(res.data)
+        setDetails((prev) => ({
+          ...prev,
+          data: res.data
+        }));
+        // alert(res.data.name)
       })
   }
 
-  return { details, getDetails };
+  return { details, getDetails, showModal };
 }
