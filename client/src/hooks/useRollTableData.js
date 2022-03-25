a3e6b87d2895e73c4fac89a2b9eb70b5e123a952
Jordan <jordan.benson4@gmail.com>
2022-03-25 10:01:46 -0700

    Add roll again function and add conditional for NPC state.

--- client/src/hooks/useRollTableData.js
+++ client/src/hooks/useRollTableData.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import kebabcase from 'lodash.kebabcase';
+import FCG from "fantasy-content-generator";
+import npbff from "npbff";
 
 
 // Custom hook for handling state between different roll table categories
@@ -9,38 +11,55 @@ export default function useRollTableData() {
     index: '',
     tableLength: 10
   });
-  const [categoryItems, setCategoryItems] = useState();
+  const [categoryItems, setCategoryItems] = useState([]);
 
   const setTableCategory = (event) => {
-    const uri = kebabcase(event.target.innerText);
-    setCategory((prev) => ({
-      ...prev,
-      index: uri,
-    }));
+    if (event.target.innerText === "Roll Again") {
+      setCategory((prev) => ({
+        ...prev,
+      }));
+    } else {
+      const uri = kebabcase(event.target.innerText);
+      setCategory((prev) => ({
+        ...prev,
+        index: uri,
+      }));
+    }
   };
 
   const setTableLength = (event) => {
-    const length = event.target.value;
+    const length = Number(event.target.value);
     setCategory((prev) => ({
       ...prev,
       tableLength: length
     }));
-  }
+  };
+
   
   useEffect(() => {
-    
-    axios.get(`https://www.dnd5eapi.co/api/${category.index}`)
-    .then((res) => {
-      const array = res.data.results;
-      const items = [];
+    if (category.index === 'np-cs') {
+    const items = [];
       while (items.length < category.tableLength) {
-        const randomElement = array[Math.floor(Math.random() * array.length)]
-        items.push(randomElement);
+        const npc = npbff();
+        items.push(npc);
       }
       setCategoryItems(items);
+    } else if (category.index !== '') {
+      axios.get(`https://www.dnd5eapi.co/api/${category.index}`)
+      .then((res) => {
+        const array = res.data.results;
+        const items = [];
+
+        while (items.length < category.tableLength) {
+          const randomElement = array[Math.floor(Math.random() * array.length)];
+          items.push(randomElement);
+        }
+        
+        setCategoryItems(items);
       })
       .catch((err) => console.log(err))
-    }, [category]);
+    }
+  }, [category]);
 
-  return { category, categoryItems, setTableCategory, setTableLength };
+  return { category, setCategory, categoryItems, setTableCategory, setTableLength };
 }
