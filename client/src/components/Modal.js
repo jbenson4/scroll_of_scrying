bc001f10b47cbe6eeb47515c793526f3439e8d7d
Jordan <jordan.benson4@gmail.com>
2022-03-25 19:36:44 -0700

    Add HTML code for magic items details.

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -2,12 +2,32 @@ import React, { Fragment } from "react";
 import './Modal.scss';
 
 const Modal = (props) => {
+  const descItems = (arr) => {
+    return arr.map((element) => <li>{element}</li>)
+  }
   return (
     <div className="modal-main" id="modal" onClick={() => {
       props.showModal();
     }}>
-      {props.show && props.category === 'monsters' && <div className="content">The {props.details.name} has {props.details.hit_points} hit points.</div>}
-      {props.show && props.category === 'magic-items' && <div className="content">The {props.details.name} has a equipment category of {props.details.equipment_category.name} and {props.details.desc.join('')}.</div>}
+      {props.show && props.category === 'monsters' && 
+      <div className="content">
+        <span>
+        The {props.details.name} has {props.details.hit_points} hit points.
+        
+        </span>
+      </div>}
+      
+      {props.show && props.category === 'magic-items' &&
+      <div className="content">
+        <h2>{props.details.name}</h2>
+        <span>Category: {props.details.equipment_category.name}</span>
+        <br></br>
+        <span>
+          <ul>
+            {props.details.desc && descItems(props.details.desc)}
+          </ul>
+        </span>
+      </div>}
     </div>
   )
 }
