e28e1f2eae5dc961291f7c6da5979e5901a47885
Jordan <jordan.benson4@gmail.com>
2022-03-26 20:54:08 -0700

    Add party conditions to state.

--- client/src/providers/PartyProvider.js
+++ client/src/providers/PartyProvider.js
@@ -6,16 +6,19 @@ export const PartyContext = createContext();
 export default function PartyProvider(props) {
   const [state, setState] = useState({
     players: [],
-    items: []
+    items: [],
+    conditions: []
   })
 
   useEffect(() => {
     Promise.all([
       axios.get("/party"),
       axios.get("/party/items"),
+      axios.get(`party/conditions`),
     ]).then((all) => {
       setState(prev => ({...prev, players: all[0].data, 
-        items: all[1].data, 
+        items: all[1].data,
+        conditions: all[2].data
       }));
     })
   }, []);
