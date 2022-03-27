8a25addcbae87adbcc8225eb61a0b11b8acc7c22
Jordan <jordan.benson4@gmail.com>
2022-03-26 18:12:51 -0700

    Remove item description from queries.

--- server/db/insertions/item_insertion.js
+++ server/db/insertions/item_insertion.js
@@ -1,7 +1,7 @@
 const addPartyItem = function (values, pool) {
   return pool.query(`
-    INSERT INTO items (index, name, description)
-    VALUES ($1, $2, $3)
+    INSERT INTO items (index, name)
+    VALUES ($1, $2)
     RETURNING *;
   `, values)
 };
