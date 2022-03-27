8366476cbb3f523fcb207f48bfd6eea61a973fe4
Jordan <jordan.benson4@gmail.com>
2022-03-26 20:59:01 -0700

    Map over player conditions passed in with state.

--- client/src/components/Player/index.js
+++ client/src/components/Player/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './Player.scss';
 import { ReactComponent as BarbarianIcon } from '../images/barbarian_icon.svg';
 import { ReactComponent as DruidIcon } from '../images/druid_icon.svg';
@@ -8,7 +8,6 @@ import { ReactComponent as RangerIcon } from '../images/ranger_icon.svg';
 import { ReactComponent as WizardIcon } from '../images/wizard_icon.svg';
 import Condition from './Condition';
 import UseVisualMode from '../../hooks/UseVisualMode';
-import axios from 'axios'
 import Edit from './Edit';
 
 const getClassIcon = (className) => {
@@ -23,13 +22,11 @@ const getClassIcon = (className) => {
   return classIcons[className];
 }
 
-
-
 const Player = (props) => {
   const EMPTY = "EMPTY";
   const EDIT = "EDIT";
   const SHOW = "SHOW";
-  const { id: playerId, name, dnd_class, stats, level, getDetails, setCategory } = props;
+  const { id: playerId, name, dnd_class, stats, level, getDetails, setCategory, playerCondition } = props;
   const [conditions, setConditions] = useState([]);
   const { mode, transition, back } = UseVisualMode(
     EMPTY
@@ -37,20 +34,13 @@ const Player = (props) => {
     const onEdit = () => {
       transition(EDIT);
     }
-    
-  useEffect(() => {
-      axios.get(`players/conditions/${playerId}`)
-      .then(res => {
-        if (res.data !== '') setConditions(res.data);
-      });
-  }, []);
 
   return (
     <article className="Player">
       {getClassIcon(dnd_class)}
       <h1>{name} | lvl {level}</h1>
       <div className="conditions">
-        {conditions !== undefined && conditions.map(condition => <Condition key={name + "_" + condition.index} getDetails={getDetails} setCategory={setCategory} {...condition}/>)}
+        {conditions !== undefined && playerCondition.map(condition => <Condition key={name + "_" + condition.index} getDetails={getDetails} setCategory={setCategory} {...condition} />)}
       </div>
       <button onClick={onEdit}>Details</button>
       {mode === EDIT && (
