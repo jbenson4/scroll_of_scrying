ef23fcd087901e3c2efd69f79d86f0dc19f7ff51
Jordan <jordan.benson4@gmail.com>
2022-03-27 12:16:20 -0700

    Add more details to NPC table.

--- client/src/components/TableRow.js
+++ client/src/components/TableRow.js
@@ -7,7 +7,7 @@ const TableRow = (props) => {
     <Fragment>
       {props.category.index === 'magic-items' && <li>You stumble upon a <strong onClick={props.getDetails} id={'magic-items'}>{props.name}</strong>!</li>}
       {props.category.index === 'monsters' && <li>A wild <strong onClick={props.getDetails} id={'monsters'}>{props.name}</strong> attacks you!</li>}
-      {props.category.index === 'np-cs' && <li>{props.npc}</li>}
+      {props.category.index === 'np-cs' && <li>{props.npc.character.name}, {props.npc.race.name}, {props.npc.character.alignment}, {props.npc.character.background}</li>}
     </Fragment>
   )
 }
