7da98fc5fc9ec9e5b34938449edfd622ce70eb96
Jordan <jordan.benson4@gmail.com>
2022-03-29 13:51:19 -0700

    Remove useEffect.

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,17 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import './Item.scss';
 
 
 const Item = (props) => {
-  const { index, name } = props;
-  const [item, setItem] = useState(null);
-  
-  useEffect(() => {
-    axios.get(`https://www.dnd5eapi.co/api/magic-items/${index}`)
-    .then(res => setItem(res.data));
-  }, []);
-
+  const { name } = props;
   return (
     <div className="Magic-Item">
       <h2 id='magic-items' onClick={props.getDetails}> { name } </h2>
